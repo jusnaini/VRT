20e38ee7efb7ee453f6dcf78e2080fc311c3daf8
jusnaini <jusnainimuslimin@gmail.com>
2019-03-20 13:35:27 +0800

    read serial data from Crop Circle sensor

---
+++ C Codes/read_sensor.c
@@ -0,0 +1,83 @@
+/*
+VERSION   : 1 (2/5/2017)
+OPERATION : Grab serial data from port and display on screen 
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <stdlib.h>
+
+int die(char *s)
+{
+    perror(s);
+    return(-1);
+}
+
+int set_interface_attribs (int fd, int speed)
+{
+    struct termios options;
+  
+    /*Get current serial port settings*/
+    if(tcgetattr(fd,&options)<0)
+        die("tcgetattr::Unable to get port settings\n");
+    /*--------------------*/
+    /*Modify port settings*/ //Noparity,8bits,1stopbit
+    /*--------------------*/
+    cfsetispeed(&options, (speed_t)speed); //B38400
+    cfsetospeed(&options, (speed_t)speed); //B38400
+    /*control options*/
+    options.c_cflag |=  ( CS8 |CLOCAL | CREAD | CRTSCTS );
+    options.c_cflag &= ~(PARENB | CSTOPB); //no parity, 1 stop bit
+    /*line options*/
+    options.c_lflag |= (ICANON);
+    /*output options*/
+    options.c_oflag = 0;
+    /*input options*/
+    options.c_iflag |=  (IGNPAR | ICRNL);
+    /*control characters*/
+    options.c_cc[VMIN]  = 1;
+    options.c_cc[VTIME] = 0;
+    
+    /*clean modem line and activate port settings*/
+    tcflush(fd,TCIFLUSH);
+    if (tcsetattr(fd, TCSANOW, &options) != 0) 
+       die("tcsetattr::Unable to set port settings\n");
+    return(-1);
+}
+
+int main(void)
+{
+    char *portname = "/dev/ttyS5";
+    int fd, rdlen;
+    unsigned char buf[255];
+    
+    /*Open port*/
+    fd = open(portname, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd < 0)
+	die("open_port: Unable to open /dev/ttyS7 \n");
+    else
+	fcntl(fd,F_SETFL,0);
+    
+    /*get and set the options*/
+    set_interface_attribs(fd,B38400);
+
+    /*simple noncanonical input*/
+    memset(buf,'\0',sizeof(buf));
+    do{
+	rdlen = read(fd,buf,sizeof(buf));
+	buf[rdlen]= 0; //to skip space after string data rcv from sensor
+
+	if((int)strlen(buf)>1){
+	   //printf("Read %d: %s\n",rdlen,buf);
+	   puts(buf);
+	}
+	sleep(0.5);
+	
+      }while(1);
+      close(fd);
+return 0;
+}	
