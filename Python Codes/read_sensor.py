cfe75160e4fcebecd9f1afb88354d723dbc001bd
jusnaini <jusnainimuslimin@gmail.com>
2019-03-20 11:51:12 +0800

    read serial data from Crop Circle sensor

---
+++ Python Codes/read_sensor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+--------------------------------------------------------------------------------------
+Created on  : 20 March 2019
+Author      : Jusnaini
+Descriptions:
+         Read data from CropCircle sensor
+         Serial port command to display data on terminal:
+                sudo chmod o+rw /dev/ttyS#
+                sudo stty -F /dev/ttyS# 38400 raw -echo
+                sudo cat /dev/ttyS#
+--------------------------------------------------------------------------------------
+"""
+
